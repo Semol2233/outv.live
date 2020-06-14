77e14d27927f698326d4648da632e2af562e3d14
Powergrapher Entertainment <semolmahamud2233@gmail.com>
2020-06-14 13:15:40 +0600

    jdfcssh

--- voutv/urls.py
+++ voutv/urls.py
@@ -9,11 +9,11 @@ from django.conf import settings
 urlpatterns = [
 
     path('', views.rootapiview.as_view()),
-    path('photo', views.photo_views.as_view()),
-    path('Academic_Info', views.Academic_Infos_views.as_view()),
-    path('livetvfedd', views.livtv_seri_views.as_view()),
-    path('coverimg', views.coverimg_seri_views.as_view()),
-    path('classnote', views.class_note_seri_views.as_view()),
+    path('sdvcsdcdcsdfder43543wrfwefcsf/photo', views.photo_views.as_view()),
+    path('sdvcsdcdcsdfder43543wrfwefcsf/Academic_Info', views.Academic_Infos_views.as_view()),
+    path('sdvcsdcdcsdfder43543wrfwefcsf/livetvfedd', views.livtv_seri_views.as_view()),
+    path('sdvcsdcdcsdfder43543wrfwefcsf/coverimg', views.coverimg_seri_views.as_view()),
+    path('sdvcsdcdcsdfder43543wrfwefcsf/classnote', views.class_note_seri_views.as_view()),
 
 
  
