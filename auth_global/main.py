de8222f9c3ea904036abba8251cf21d4a75d7d77
Powergrapher Entertainment <semolmahamud2233@gmail.com>
2020-06-16 19:37:50 +0600

    duhssh

--- auth_global/main.py
+++ auth_global/main.py
@@ -1,10 +1,10 @@
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework_simplejwt.authentication.JWTAuthentication',
+    #    'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
     # 'DEFAULT_PAGINATION_CLASS':(
