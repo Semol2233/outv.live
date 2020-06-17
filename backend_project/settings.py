8226832fff265e99edf99f4205bd78db2f7e52ee
Powergrapher Entertainment <semolmahamud2233@gmail.com>
2020-06-17 20:19:00 +0600

    dusfssh

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -29,10 +29,10 @@ SECRET_KEY = '$9qjq3$sr73rn&*l5-lal@2#l9-g-5d$75gwv+yox1*z)1u_4x'
 #DEBUG = False production_server
 #DEBUG = False production_server
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['cdn.boutv.live']
-#ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = ['cdn.boutv.live']
+ALLOWED_HOSTS = []
 #f
 #d
 # Application definition
