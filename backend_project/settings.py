10654c2473afb996be9249f40f753cf1ad414445
Powergrapher Entertainment <semolmahamud2233@gmail.com>
2020-06-28 22:55:37 +0600

    dhcfhgssssd

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -83,11 +83,13 @@ SITE_ID=1
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
-CORS_ORIGIN_ALLOW_ALL = False
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     'www.boutv.live',
     'boutv.live',
+    'http://www.boutv.live/'
 
 
 )
